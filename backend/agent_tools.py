7f82b0c867eb8bd36f81b951969951aa93f81d04
Julian Boronat <julian.boronat@mongodb.com>
2025-03-25 16:50:12 +0100

    Fixing wrong stuff, need to continue checking...
    Fixing wrong stuff, need to continue checking...

--- backend/agent_tools.py
+++ backend/agent_tools.py
@@ -34,41 +34,44 @@ vector_store = create_vector_store(
 @tool
 def fetch_guidelines(query: str, n=1) -> str:
     """Runs semantic search on existing policies to find relevant ones based on the image description."""
-    result = vector_store.similarity_search_with_score(query=query, k=n)
-    
+    result = vector_store.similarity_search_with_score(query=query, k=n) 
+    print("Vector store - Similarity Search Raw: ", result)
+    print("Vector store - Similarity Search Partial: ", str(result[0][0].page_content))
     return str(result[0][0].page_content)
-    #return str(result)
 
 @tool
-def persist_data(data):
+def persist_data(data) -> dict:
     """Persists the data in the database."""
-
-    # Ensure required fields are present
-    # required_fields = ["accident_description", "timestamp", "final_answer"]
-    # for field in required_fields:
-    #     if field not in data:
-    #         return f"Error: Missing required field '{field}'"
-
-    # # Convert timestamp if necessary
-    # if isinstance(data["timestamp"], str):
-    #     try:
-    #         data["timestamp"] = datetime.fromisoformat(data["timestamp"].replace("Z", "+00:00"))
-    #     except ValueError:
-    #         return "Error: Invalid timestamp format. Use ISO 8601 (e.g., '2021-07-27T22:00:00Z')"
-    
-
     cluster_uri = os.getenv("MONGODB_URI")
     database_name = os.getenv("DATABASE_NAME")
     collection_name = os.getenv("COLLECTION_NAME_2")
 
     client = MongoClient(cluster_uri)
     db = client[database_name]
+    
+    # Persist data
     collection = db[collection_name]
-
     collection.insert_one(data)
-    return "Data persisted successfully."
 
-tools = [fetch_guidelines, persist_data]
+    return {"message": "Data persisted successfully."}
+
+@tool
+def clean_chat_history() -> dict:
+    """Cleans the chat history in the database at the end of the workflow."""
+    cluster_uri = os.getenv("MONGODB_URI")
+    database_name = os.getenv("DATABASE_NAME")
+    chat_history_coll = os.getenv("CHAT_HISTORY_COLLECTION")
+
+    client = MongoClient(cluster_uri)
+    db = client[database_name]
+    
+    # Persist data
+    collection = db[chat_history_coll]
+    collection.delete_many({})
+
+    return {"message": "Chat history cleaned successfully."}
+
+tools = [fetch_guidelines, persist_data, clean_chat_history]
 
 if __name__ == "__main__":
     # Example usage
